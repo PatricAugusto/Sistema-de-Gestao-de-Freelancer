f04a0e0dc47cc0a841fdb280ff6480eb7333e15d
Patric Augusto <druidacanabico@gmail.com>
2026-03-28 17:08:01 -0300

    feat(database): adiciona configuração de produção com SQLite

--- api/knexfile.js
+++ api/knexfile.js
@@ -8,5 +8,11 @@ module.exports = {
       directory: './src/database/migrations'
     },
     useNullAsDefault: true,
+  },
+  production: {
+    client: 'sqlite3',
+    connection: { filename: '/opt/render/project/src/database/database.db' },
+    useNullAsDefault: true,
+    migrations: { directory: './src/database/migrations' }
   }
 };
